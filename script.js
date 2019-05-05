5c909f66edc1b2cbf22cdfd393802658f1f58eaf
Jose Horta <joshhortt@yahoo.com>
2019-05-05 13:02:36 +0100

    add comments to script.js: refactoring new variable and forEach() method looping every array input element.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $("#blanks form").submit(function() {
-// START REFACTOR CODE
-    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
+// START REFACTOR CODE
+
+// Only one General variable with an Array of all Inputs in strings.
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+// add forEach() method to loop each array element
     blanks.forEach(function(blank) {
       var userInput = $("input#" + blank).val();
       $("." + blank).text(userInput);
     });
+    
 // END REFACTOR CODE
     $("#story").show();
 
